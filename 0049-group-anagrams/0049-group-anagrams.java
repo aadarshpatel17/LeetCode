84a751d551be99f5749defdaa3f52d2bbd31e912
Aadarsh Patel <71113410+aadarshpatel17@users.noreply.github.com>
2026-07-08 08:33:02 +0530

    Time: 1226 ms (5.05%), Space: 49.2 MB (96.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,44 +5,36 @@ class Solution {
         List<List<String>> result = new ArrayList<>();
         boolean[] used = new boolean[len];
 
-        for (int i = 0; i < len; i++) {
-            List<String> anagrams = new ArrayList<>();
-            if (!used[i]) {
-                anagrams.add(strs[i]);
+        for (int i = 0; i < strs.length; i++) {
+            if (used[i]) {
+                continue;
             }
+
+            List<String> anagrams = new ArrayList<>();
+            anagrams.add(strs[i]);
             used[i] = true;
             for (int j = i + 1; j < len; j++) {
-                if (!used[j] && isAnagram(strs[i], strs[j])) {
+                if (!used[j] && isAnagram_optimized(strs[i], strs[j])) {
                     anagrams.add(strs[j]);
                     used[j] = true;
                 }
             }
-            if (!anagrams.isEmpty())
-                result.add(anagrams);
+            result.add(anagrams);
         }
 
         return result;
     }
 
-    public boolean isAnagram(String a, String b) {
+    public boolean isAnagram_optimized(String a, String b) {
         if (a.length() != b.length()) {
             return false;
         }
+        return sortString(a).equals(sortString(b));
+    }
 
-        boolean[] used = new boolean[b.length()];
-        for (int i = 0; i < a.length(); i++) {
-            boolean found = false;
-            for (int j = 0; j < b.length(); j++) {
-                if (!used[j] && a.charAt(i) == b.charAt(j)) {
-                    used[j] = true;
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-                return false;
-        }
-
-        return true;
+    public String sortString(String str) {
+        char[] ch = str.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
     }
 }
