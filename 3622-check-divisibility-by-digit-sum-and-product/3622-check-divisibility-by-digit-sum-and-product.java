83e9b3756286f96a4b550275d0b2d5b4aaaa94fd
Aadarsh Patel <71113410+aadarshpatel17@users.noreply.github.com>
2026-08-22 21:29:55 +0530

    Time: 0 ms (100%), Space: 42.4 MB (42.42%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -1,27 +1,20 @@
 class Solution {
     public boolean checkDivisibility(int n) {
-        return n % (digitSum(n) + digitProduct(n)) == 0;
+        return n % digitCal(n) == 0;
     } 
 
     // digit sum
-    public int digitSum(int n) {
+    public int digitCal(int n) {
         int sum = 0;
-        while (n > 0) {
-            int ld = n % 10;
-            sum += ld;
-            n /= 10;
-        }
-        return sum;
-    }
-
-    // digit product
-    public int digitProduct(int n) {
         int product = 1;
         while (n > 0) {
             int ld = n % 10;
+            sum += ld;
             product *= ld;
             n /= 10;
         }
-        return product;
+        return sum + product;
     }
+
+    // digit product
 }
