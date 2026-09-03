0d62f70cfe1861641e7e9e24488cffceb0ac4572
Aadarsh Patel <71113410+aadarshpatel17@users.noreply.github.com>
2026-09-03 23:25:57 +0530

    Time: 8 ms (50%), Space: 121.7 MB (62.32%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean uniformArray(int[] nums) {
+
+        int minOdd = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                minOdd = Math.min(minOdd, num);
+            }
+        }
+
+        // All numbers are even
+        if (minOdd == Integer.MAX_VALUE) {
+            return true;
+        }
+
+        // Every even number must have a smaller odd number
+        for (int num : nums) {
+            if (num % 2 == 0 && num <= minOdd) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
